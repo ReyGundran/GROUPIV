da1e35897f4db3736c044e466bf283996683d787
potpot <potpot>
2016-03-21 16:07:56 +0800

    my_crud.py

---
+++ my_crud.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'my_crud.ui'
+#
+# Created: Mon Mar 21 11:59:21 2016
+#      by: PyQt4 UI code generator 4.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
